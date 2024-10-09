8ff5e9c89bc9f3118fcd525198c0a5faacb29535
Arturo <asuarezm23@fpcoruna.afundacion.org>
2024-10-09 09:29:09 +0200

    Completada 1a Tarea (Suma, Resta, Multiplicación y División)

--- sprint0python/pythonProject/operaciones.py
+++ sprint0python/pythonProject/operaciones.py
@@ -1,14 +1,31 @@
-suma = 3+5
-resta = 9-4
-multiplicacion = 5*2
-division = 13/2
+#SUMA, RESTA, MULTIPLICACIÓN, DIVISIÓN
 
-print("El resultado de la suma es: ", suma)
-print("El resultado de la resta es: ", resta)
-print("El resultado de la multiplicación es: ", multiplicacion)
 
-try:
-    print("El resultado de la división es: ", division)
-except ZeroDivisionError:
-    print("No se puede dividir por cero")
+#FUNCIÓN SUMA
+def suma (n1,n2):
+    return n1 + n2
+
+#FUNCIÓN RESTA
+def resta (n1,n2):
+    return n1 - n2
+
+#FUNCIÓN MULTIPLICACIÓN
+def multiplicacion (n1,n2):
+    return n1 * n2
+
+#FUNCIÓN DIVISIÓN
+#Controlo la división por 0
+def division (n1, n2):
+    try:
+        return n1 / n2
+    except ZeroDivisionError:
+        print("No se puede dividir por cero")
+
+print("El resultado de la suma es: ", suma(2,4))
+print("El resultado de la resta es: ", resta(8,3))
+print("El resultado de la multiplicación es: ", multiplicacion(3,4))
+print("El resultado de la división es: ", division(2,0))
+
+
+
 
