52d400fb41e0c3121b931b5f13817d56155e761b
Arturo <asuarezm23@fpcoruna.afundacion.org>
2024-10-21 12:08:14 +0200

    Completada tarea 11

--- sprint1Tkinter/pythonProject1/Ejercicio_11.py
+++ sprint1Tkinter/pythonProject1/Ejercicio_11.py
@@ -6,11 +6,32 @@ Crea una barra deslizante (Scale) que permita al usuario seleccionar un número 
 
 import tkinter as tk
 
+
 # Creamos la ventana principal
 
 root = tk.Tk()
 root.title("Scale")
-root.geometry("300x300")
+root.geometry("300x150")
 
 
-#
+# Actualizarr valor
+
+def actualizar_valor(valor):
+    etiqueta.config(text=f"Valor: {valor}")
+
+
+# Etiqueta para mostrar el valor
+
+etiqueta = tk.Label(root, text="Valor: 0")
+etiqueta.pack(pady=10)
+
+
+# Crear Scale
+
+scale = tk.Scale(root, from_ = 0, to = 100, orient = 'horizontal', command=actualizar_valor)
+scale.pack(pady=10)
+
+
+# Bucle principal
+
+root.mainloop()
