34c774a1979950ea39ec6e2d6a3bb40bc585da6f
Arturo <asuarezm23@fpcoruna.afundacion.org>
2024-11-26 17:49:38 +0100

    Completada tarea 2

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,6 +1,9 @@
 package com.example.mycatalog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,14 @@ public class CatalogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_catalog);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button navigateButton = findViewById(R.id.btn_navigate_to_detail);
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lanzar DetailActivity
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
